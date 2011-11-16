834142ff1becef566ae48215b8ed5a343747502b
Dmitry Kurinskiy <name.alari@gmail.com>
2011-11-16 19:33:04 +0400

    initial commit

---
+++ test/unit/ru/mirari/image/MirariImageStorageServiceTests.groovy
@@ -0,0 +1,17 @@
+package ru.mirari.image
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(MirariImageStorageService)
+class MirariImageStorageServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
